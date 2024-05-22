4780ff3eeaf5be49cd348f0a6040ebac46d4fbcf
Nikki Franco <nfranco2@travelers.com>
2024-05-22 09:08:03 -0400

    added some basic middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
 import express from 'express';
 
-
+dotenv.config(); 
 const app = express(); 
-
-const PORT = 3000; 
-
+app.use(cors());
+app.use(express.json());
+const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('welcome to the record shop!')
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Figure out what routes to include for records.
+
+app.get('/search', (req, res) => { 
+    res.status(200).send('placeholder, sends matching products')
+});
+
+app.get('/search/:category', (req, res) => {
+    res.status(200).send(`placeholder, sends matching products to specific category`)
+})
+
+
+app.post("/checkout", (req, res)=> {
+    // From documentation: 
+    // When the user is finished shopping, the checkout
+    // view will show them all the products in their cart,
+    // an order total, and a place for them to put their
+    // payment and shipping info. When they checkout, their cart
+    // is cleared, and the order is saved to the database.
+
+
+    // In request body, take in list of products from cart, order total (accumulated)
+    // and user input for payment info(?) and shipping information.
+    res.status(200).send('Order sent to database!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
