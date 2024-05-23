70f77964efb2a57c22821e687c1c02325add170d
Nikki Franco <nfranco2@travelers.com>
2024-05-22 23:08:07 -0400

    created record details component

--- react/src/components/RecordDetails.jsx
+++ react/src/components/RecordDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import recordImage from '../assets/record.svg'
 import {
     useParams,
+    Link
 } from "react-router-dom";
 
 
@@ -12,7 +14,7 @@ export default function RecordDetails(props) {
             try {
                 const response = await fetch(`http://localhost:3000/records/${id}`);
                 if (!response.ok) {
-                  throw new Error('Data could not be fetched!');
+                    throw new Error('Data could not be fetched!');
                 }
                 const json_response = await response.json();
                 setRecord(json_response); // assign JSON response to the data variable.
@@ -23,9 +25,30 @@ export default function RecordDetails(props) {
         fetchData();
     }, []);
 
+    function addToCart() { 
+        // implement logic for adding product to cart.
+        alert('added to cart!');
+    }
+
     return (
-        <div>
-            <h1 id="name">{record.album_name}</h1>
+        <div className="card mb-3" style={{maxWidth: "1000px"}}>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={recordImage} className="img-fluid" />
+                </div>
+                <div className="col-md-8">
+                    <div class="card-body">
+                        <h3 className="card-title" style={{textTransform: "uppercase"}}><strong>{record.album_name}</strong></h3>
+                        <p className="card-text"><strong>{record.band_name}</strong></p>
+                        <p className="card-text"><small className="text-muted">{record.release_date}</small></p>
+                        <p className="card-text">Genre: {record.genre}</p>
+                        <p className="card-text">Record Label: {record.record_label}</p>
+                        <p className="card-text">Duration: {record.duration}</p>
+                        <button className="btn btn-primary" onClick={addToCart}>Add to cart!</button> 
+                        <h5>{record.price}</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
